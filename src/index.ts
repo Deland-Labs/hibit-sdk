3946adb10253cbaf7135b2dae16bcffc257449d5
witter-deland <87846830+witter-deland@users.noreply.github.com>
2025-03-08 21:08:51 +0800

    fix: exports (#59)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import 'reflect-metadata';
 
 export * from './types';
+export { hibitClient } from './hibit-client';
+export { TransactionManager } from './tx-manager';
+export { HibitClientError } from './error';
