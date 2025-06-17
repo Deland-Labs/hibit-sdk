c21f1b66afefc2c9189f6c32cd430f345f92439a
Witter <varik1770@gmail.com>
2025-06-17 16:05:01 +0800

    fix: enhance exports for Hibit client and wallet API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 export * from './types';
-export { hibitClient } from './hibit-client';
+export type { HibitApiOptions } from './hibit-client';
+export { hibitClient, HibitClient } from './hibit-client';
 export { TransactionManager } from './tx-manager';
 export { HibitError } from './error';
 export { Keypair } from './Keypair';
-export type { IWalletApi } from './wallet-api';
+export type { IWalletApi, MetaMaskWalletApi, KaswareWalletApi } from './wallet-api';
+export { brokerClient, BrokerClient } from './broker-client';
