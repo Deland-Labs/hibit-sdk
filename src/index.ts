f838a8670b841305d172d9649767563fdd984acf
witter-deland <87846830+witter-deland@users.noreply.github.com>
2025-06-17 17:55:02 +0800

    fix: exports (#101)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import 'reflect-metadata';
 export * from './types';
-export type { HibitApiOptions } from './hibit-client';
-export { hibitClient, HibitClient } from './hibit-client';
-export { TransactionManager } from './tx-manager';
-export { HibitError } from './error';
-export { Keypair } from './Keypair';
-export type { IWalletApi, AbstractWalletApi, MetaMaskWalletApi, KaswareWalletApi } from './wallet-api';
-export { brokerClient, BrokerClient } from './broker-client';
+export * from './hibit-client';
+export * from './tx-manager';
+export * from './error';
+export * from './Keypair';
+export * from './wallet-api';
+export * from './broker-client';
